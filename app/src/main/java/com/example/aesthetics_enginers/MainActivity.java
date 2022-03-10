040bbe8fb18bf650a747ebeaa96eaa9fad22f2a5
Gheorghe Bujac <bujacg@cardiff.ac.uk>
2022-03-10 16:06:35 +0000

    Implemented the navigation drawer

--- app/src/main/java/com/example/aesthetics_enginers/MainActivity.java
+++ app/src/main/java/com/example/aesthetics_enginers/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.aesthetics_enginers.User_Account.Login;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +17,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity{
     private FirebaseFirestore mDb;
+    private DrawerLayout drawer;
+
     Button Button_Sign_Out;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +34,32 @@ public class MainActivity extends AppCompatActivity{
                 signOut();
             }
         });
+
+
+        //Nav Menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
     }
 
 
-     private void signOut(){
+
+    private void signOut(){
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, Login.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
